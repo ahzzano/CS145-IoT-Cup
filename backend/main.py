807a61d19ccf6072c1322658022ebf5414504b13
W Santos <51366379+ahzzano@users.noreply.github.com>
2026-04-29 15:01:10 +0800

    feat: add basic server

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
+from flask import Flask
+from flask_restx import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
 def main():
     print("Hello from backend!")
-
+    app.run(debug=True)
 
 if __name__ == "__main__":
     main()
