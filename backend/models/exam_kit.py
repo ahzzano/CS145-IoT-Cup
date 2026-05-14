0f575584482082d6c3d570041f1b752ae7ee27c3
W Santos <51366379+ahzzano@users.noreply.github.com>
2026-05-14 09:12:49 +0800

    feat: add db upgrades

--- backend/models/exam_kit.py
+++ backend/models/exam_kit.py
@@ -5,7 +5,7 @@ from models.base import db
 class ExamKit(db.Model):
     kit_id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True, init=False)
     submitted: Mapped[bool]
-    examinee_id: Mapped[int] = mapped_column(ForeignKey("examinee.id"), nullable=True)
+    examinee_id: Mapped[int] = mapped_column(ForeignKey("examinee.id", ondelete='SET NULL'), nullable=True)
 
     def to_dict(self):
         return { c.name: getattr(self, c.name) for c in self.__table__.columns }
