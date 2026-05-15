3aa46945e0fa97ee7810fe1d5229d3ba50f0d5bf
NikYT05 <rvtan1@up.edu.ph>
2026-05-15 18:21:09 +0800

    feat: Added servoMotor functionality

--- hardware/ServoMotor.cpp
+++ hardware/ServoMotor.cpp
@@ -1,14 +1,15 @@
 #include <Servo.h>
 
 int pos = 0;
-int signal = 2; //This is the pin where we will get input from ESP32 for granting access
+int tact_input = 2; //This is the pin where we will get input from the tact switch
+int callback   = LOW;
 
 Servo servo;
 
 void setup()
 {
   servo.attach(9, 500, 2500);
-  pinMode(signal, INPUT);
+  pinMode(tact_input, INPUT);
 
   servo.write(pos);
   delay(500);
@@ -16,15 +17,16 @@ void setup()
 
 void loop()
 {
-  bool granted = digitalRead(signal);
-  if (granted){
-    Serial.print("granted");
+  bool pressed = digitalRead(tact_input);
+  if (pressed){
+    Serial.print("pressed");
     servo.write(90);
-    delay(5000);
+    delay(2000);
     servo.write(0);
+    digitalWrite(callback, HIGH);
+      delay(2000);
   }
-}
 
-// We will isolate the servo motor control to an Arduino.
-// The ESP32 will send a signal to the Arduino when access is granted, and the Arduino will move the servo motor to open the door.
+  digitalWrite(callback, LOW);
+}
 
