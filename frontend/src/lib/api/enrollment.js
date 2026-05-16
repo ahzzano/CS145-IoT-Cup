be62dc3ec0701c12522e1ade11497be3b86ad73a
W Santos <51366379+ahzzano@users.noreply.github.com>
2026-05-16 14:32:21 +0800

    feat: add credentials include to api

--- frontend/src/lib/api/enrollment.js
+++ frontend/src/lib/api/enrollment.js
@@ -14,7 +14,9 @@ const DEFAULT_API_BASE_URL = 'https://veritest.duckdns.org'
 async function getExamineeImage(img_path) {
     const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || DEFAULT_API_BASE_URL;
     console.log(`${apiBaseUrl}/${img_path}`)
-    const response = await fetch(`${apiBaseUrl}/${img_path}`)
+    const response = await fetch(`${apiBaseUrl}/${img_path}`, {
+        credentials: 'include'
+    })
 
     if (!response.ok) {
         return null
