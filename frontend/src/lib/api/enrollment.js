c9fe90072cde877706ad73cb9e2684d1064c3202
W Santos <51366379+ahzzano@users.noreply.github.com>
2026-05-17 18:21:02 +0800

    feat: add better kyc with bypass

--- frontend/src/lib/api/enrollment.js
+++ frontend/src/lib/api/enrollment.js
@@ -1,7 +1,8 @@
-import { PUBLIC_BYPASS } from '$env/static/public'
+import { PUBLIC_BYPASS, PUBLIC_DEFAULT_API_BASE_URL } from '$env/static/public'
 // const DEFAULT_API_BASE_URL = '/api';
 // const DEFAULT_API_BASE_URL = 'http://localhost:8000'
-const DEFAULT_API_BASE_URL = 'https://veritest.duckdns.org'
+// const DEFAULT_API_BASE_URL = 'https://veritest.duckdns.org'
+const DEFAULT_API_BASE_URL = PUBLIC_DEFAULT_API_BASE_URL
 /**
  * @typedef {{ success: true; message: string; examinee_id: string }} EnrollmentSuccess
  * @typedef {{ success: false; error: string }} EnrollmentError
@@ -26,6 +27,62 @@ async function getExamineeImage(img_path) {
     }
 }
 
+async function getKycData(examinee_uin, examinee_name) {
+    const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || DEFAULT_API_BASE_URL;   
+
+    if(PUBLIC_BYPASS == 'true') {
+        console.log("BYPASS HIT")
+        const picture_blob = await fetch(`${apiBaseUrl}/serve/${examinee_uin}.jpg`, {
+            credentials: 'include'
+        })
+        return {
+            success: true,
+            uin: examinee_uin,
+            name: examinee_name,
+            picture: `${examinee_uin}.jpg`,
+            blob: await picture_blob.blob()
+        }
+    }
+    const kyc_response = await fetch(`${apiBaseUrl}/mosip/kyc`, {
+        method: 'GET',
+        cache: 'no-store',
+        credentials: 'include'
+    })
+
+    if (!kyc_response.ok) {
+        return {
+            success: false,
+            error: 'Unable to enroll. Please try again.'
+        };
+    }
+
+    const kyc_json = await kyc_response.json().catch(() => ({}))
+
+    const pic = kyc_json.data.photo_path
+    const name= kyc_json.data.name
+    const uin = kyc_json.uin
+
+    let examinee_picture_fname = pic.split("/")[1]
+    console.log(examinee_picture_fname)
+
+    let picture_blob = await getExamineeImage(pic)
+
+    if(!picture_blob) {
+        return {
+            success: false, 
+            error: 'Unable to retrieve enrollee picture'
+        }
+    }
+    
+    return {
+        success: true,
+        uin: uin,
+        name: name,
+        picture: examinee_picture_fname,
+        blob: picture_blob
+    }
+}
+
 /**
  * Sends QR image for enrollment validation and registration.
  * @param {File} qrImage
@@ -36,12 +93,14 @@ export async function enrollExaminee(qrImage) {
     const auth_form_data = new FormData();
     auth_form_data.append('file', qrImage);
 
+    console.log('authing...')
     const auth_response = await fetch(`${apiBaseUrl}/mosip/auth`, {
         method: 'POST',
         body: auth_form_data,
         cache: 'no-store',
         credentials: 'include'
     });
+    console.log('auth done')
 
     const auth_json = await auth_response.json().catch(() => ({}));
 
@@ -53,6 +112,7 @@ export async function enrollExaminee(qrImage) {
     }
 
     const examinee_id = auth_json.data.uin
+    const examinee_name = auth_json.data.name
 
     const existing_examinee_response = await fetch(
         `${apiBaseUrl}/examinee/?id=${encodeURIComponent(examinee_id)}`,
@@ -78,41 +138,16 @@ export async function enrollExaminee(qrImage) {
         };
     }
 
-    const kyc_response = await fetch(`${apiBaseUrl}/mosip/kyc`, {
-        method: 'GET',
-        cache: 'no-store',
-        credentials: 'include'
-    })
-
-    if (!kyc_response.ok) {
+    console.log('kyc-ing...')
+    const kyc = await getKycData()
+    if (kyc.error) {
         return {
             success: false,
-            error: auth_json?.error || 'Unable to enroll. Please try again.'
+            error: kyc.error
         };
     }
-
-    const kyc_json = await kyc_response.json().catch(() => ({}))
-    const examinee_picture = kyc_json.data.photo_path
-    const examinee_name = kyc_json.data.name
-    const examinee_uin = kyc_json.uin
-
-    let examinee_picture_fname = examinee_picture.split("/")[1]
-    console.log(examinee_picture_fname)
-
-    let picture_blob = await getExamineeImage(examinee_picture)
-    if (!picture_blob) {
-        if(PUBLIC_BYPASS == 'true') {
-            const picture = await fetch(`${DEFAULT_API_BASE_URL}/serve/${examinee_uin}.jpg`)
-            picture_blob = await picture.blob()
-            examinee_picture_fname = `${examinee_uin}.jpg`
-            console.log(examinee_picture_fname)
-        } else {
-            return {
-                success: false,
-                error: 'Unable to retrieve the verified examinee photo.'
-            };
-        }
-    }
+    const picture_blob = kyc.blob
+    const examinee_picture_fname = kyc.picture
 
     const picture_file = new File([picture_blob], examinee_picture_fname, {type: picture_blob.type})
 
